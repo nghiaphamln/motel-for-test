12866420676e0659dcc81596b21e5f3ac1d0478c
Minh Nghĩa <nghiaphamln3@gmail.com>
2023-11-28 08:24:31 +0700

    edit connectionstring

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    'url': 'mongodb+srv://nghiapham:Nghia%40123@cluster0.w6sw0.mongodb.net/?retryWrites=true&w=majority'
+    'url': 'mongodb+srv://nghiapham:Nghia%40123@cluster0.w6sw0.mongodb.net/moteltest?retryWrites=true&w=majority'
 };
